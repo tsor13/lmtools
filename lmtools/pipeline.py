b12a3b0dffe8f6120fcc153dab49e86eaaa0b481
Taylor Sorensen <taylorsorensen@Taylors-MacBook-Pro.local>
2022-03-28 10:45:22 -0600

    initial commit

---
+++ lmtools/pipeline.py
@@ -0,0 +1,94 @@
+from datetime import date
+from datetime import date
+from pdb import set_trace as breakpoint
+import os
+import argparse
+from lmtools.experiment import Experiment
+from lmtools.postprocessor import Postprocessor
+# import variable API_KEY from environment
+# API_KEY = os.environ.get("API_KEY")
+
+
+
+def run_experiment(ds_path, model_name, n=500):
+
+    # camel_case_ds_name = "".join(word.title() for word in ds_name.split('_'))
+
+    # TODO - create ds dataframes?
+    # # Create dataset
+    # print("Building dataset...")
+    # ds_cls_name = f"{camel_case_ds_name}Dataset"
+    # exec(f"from {ds_name} import {ds_cls_name}")
+    # eval(f"{ds_cls_name}(n={n})")
+
+    # Pass data through model
+    print("Passing data through model...")
+    # Experiment(model_name=model_name, ds_name=ds_name)
+    in_fname = ds_path
+    date_str = date.today().strftime("%Y-%m-%d")
+    # get directory where ds_path is located
+    # replace_str
+    replace_str = f"_exp_results_{model_name.replace('/', '-')}_{date_str}.pkl"
+    # replace .pkl with replace_str
+    out_fname = in_fname.replace(".pkl", replace_str)
+    Experiment(
+        model_name=model_name,
+        in_fname=in_fname,
+        out_fname=out_fname,
+    )
+
+    model_name = model_name.replace('/', '-')
+
+    # Postprocessing
+    print("Postprocessing...")
+    date_str = date.today().strftime("%d-%m-%Y")
+    processed_in = out_fname
+    # replace .pkl with _processed.pkl
+    processed_out = processed_in.replace('.pkl', '_processed.pkl')
+
+    Postprocessor(results_fname=processed_in,
+                  save_fname=processed_out,
+                  matching_strategy="startswith")
+
+def main():
+    parser = argparse.ArgumentParser(prog ='pipeline',)
+    # MUST be present, cannot accept none
+    parser.add_argument('-d', '--ds_path', type=str,
+                        help='path to dataset',
+                        required=True)
+    parser.add_argument('-m', '--model_name', type=str,
+                        help='path to model',
+                        required=True)
+    args = parser.parse_args()
+    ds_path = args.ds_path
+    model_name = args.model_name
+    print(f"ds_path: {ds_path}")
+    print(f"model_name: {model_name}")
+    run_experiment(ds_path, model_name)
+
+
+if __name__ == "__main__":
+    # import argparse
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("ds_path", type=str,
+    #                     help="Path to ds.pkl file")
+    # parser.add_argument("model_name", type=str,
+    #                     help="Name of model (see lmsampler for choices)")
+    # # parser.add_argument("-n", "--n", type=int,
+    # #                     help="Number of rows of data to process")
+    # args = parser.parse_args()
+    import sys
+    # sys.argv
+    ds_path = sys.argv[1]
+    model_name = sys.argv[2]
+
+    # run_experiment(ds_name=args.ds_name,
+    #                model_name=args.model_name,
+    #                n=args.n)
+
+    # run_experiment(ds_name=args.ds_name,
+    #                model_name=args.model_name)
+
+    run_experiment(ds_path=ds_path,
+                   model_name=model_name)
---
+++ setup.py
@@ -0,0 +1,43 @@
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+# __version__ = open(join(dirname(__file__), 'slackbot/VERSION')).read().strip()
+# 
+# install_requires = (
+#     # 'requests>=2.4.0',
+#     # 'websocket-client>=0.22.0,<=0.44.0',
+#     # 'slacker>=0.9.50',
+#     # 'six>=1.10.0'
+# ) 
+# 
+excludes = (
+    '*test*',
+    '*local_settings*',
+)
+
+setup(
+    name='lmtools',
+    version='0.1',
+    license='MIT',
+    description='Tools for OTR with language models.',
+    author='Taylor Sorensen',
+    author_email='tsor1313@gmail.com',
+    url='http://github.com/tsor13/lmtools',
+    packages=find_packages(exclude=excludes),
+    entry_points={
+        'console_scripts': [
+            'lm-pipeline = lmtools.pipeline:main',
+        ]
+    },
+    # install_requires=install_requires,
+    # classifiers=['Development Status :: 4 - Beta',
+    #             'License :: OSI Approved :: MIT License',
+    #             'Operating System :: OS Independent',
+    #             'Programming Language :: Python',
+    #             'Programming Language :: Python :: 2',
+    #             'Programming Language :: Python :: 2.7',
+    #             'Programming Language :: Python :: 3',
+    #             'Programming Language :: Python :: 3.4',
+    #             'Programming Language :: Python :: 3.5',
+    #             'Programming Language :: Python :: 3.6']
+)
