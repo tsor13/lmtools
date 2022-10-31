fb1120aa12cdda933cc83dc70fbd3476647aba8d
Taylor Sorensen <tsor1313@gmail.com>
2022-10-31 15:34:17 -0700

    more robust pipeline

---
+++ build/lib/lmtools/experiment.py
@@ -0,0 +1,62 @@
+from datetime import date
+
+import pandas as pd
+import tqdm
+
+from lmtools.lmsampler import LMSampler
+
+
+class Experiment:
+    def __init__(
+        self, model_name, ds_name=None, in_fname=None, out_fname=None, n_probs=100
+    ):
+
+        # Get in_fname and out_fname
+        if ds_name is not None:
+            # Use ds_name if it is available,
+            # regardless of in_fname/out_fname
+            # availability
+            in_fname = f"data/{ds_name}/ds.pkl"
+            date_str = date.today().strftime("%d-%m-%Y")
+            # replace '/' with '-'
+            out_fname = (
+                f"data/{ds_name}/exp_results_"
+                f"{model_name.replace('/', '-')}_{date_str}.pkl"
+            )
+        else:
+            if (in_fname is None) or (out_fname is None):
+                msg = "Please either specify ds_name " "OR (in_fname AND out_fname)"
+                raise RuntimeError(msg)
+            else:
+                pass
+
+        self._model_name = model_name
+        self._out_fname = out_fname
+        self._n_probs = n_probs
+
+        # Open the data for this experiment
+        self._ds_df = self._open_ds_df(in_fname)
+
+        # Create model
+        self._model = LMSampler(model_name)
+
+        # Run the experiment and save the resulting data
+        self._run()
+
+    def _open_ds_df(self, ds_fname):
+        return pd.read_pickle(ds_fname)
+
+    def _run(self):
+        resps = []
+        for _, row in tqdm.tqdm(self._ds_df.iterrows(), total=self._ds_df.shape[0]):
+            try:
+                resp = self._model.send_prompt(row["prompt"], n_probs=self._n_probs)
+                resps.append(resp)
+            except Exception as e:
+                print(e)
+                resps.append(None)
+
+        # Make new df and save it
+        self._ds_df["resp"] = resps
+        self._ds_df["model"] = [self._model_name] * len(resps)
+        self._ds_df.to_pickle(self._out_fname)
---
+++ build/lib/lmtools/lm_jurassic.py
@@ -0,0 +1,94 @@
+import os
+import time
+
+import requests
+
+from lmtools.lmsampler_baseclass import LMSamplerBaseClass
+
+
+class LM_JURASSIC(LMSamplerBaseClass):
+    def __init__(self, model_name="j1-jumbo"):
+        """
+        Supported models: 'j1-jumbo', 'j1-large'
+        """
+        super().__init__(model_name)
+        self.engine = model_name
+        # make sure engine is a valid model
+        if self.engine not in ["j1-jumbo", "j1-large"]:
+            raise ValueError(
+                "Invalid model name. Must be one of: 'j1-jumbo', 'j1-large'"
+            )
+        # make sure API key is set
+        self.API_KEY = API_KEY
+        # make sure API key is set
+        if os.environ.get("AI21_API_KEY") is None:
+            raise Exception("Please set API key as environment variable")
+        # warn only once
+        self.warned = False
+
+    def send_prompt(self, prompt, n_probs=100):
+        start_time = time.time()
+        max_jurassic_n_probs = 64
+        if n_probs > max_jurassic_n_probs:
+            n_probs = max_jurassic_n_probs
+            if not self.warned:
+                print(
+                    f"n_probs > {max_jurassic_n_probs}. Setting n_probs to {max_jurassic_n_probs}"
+                )
+                self.warned = True
+        response = requests.post(
+            f"https://api.ai21.com/studio/v1/{self.engine}/complete",
+            headers={"Authorization": "Bearer " + str(self.API_KEY)},
+            json={
+                "prompt": prompt,
+                "numResults": 1,
+                "maxTokens": 1,
+                "topKReturn": n_probs,
+                "temperature": 0.0,
+            },
+        )
+        # parse response to dictionary
+        response_dict = response.json()
+        if "completions" not in response_dict:
+            print(response_dict)
+        logprobs = response_dict["completions"][0]["data"]["tokens"][0]["topTokens"]
+        # change list of dict {token, logprob} to dictionary of {token: logprob}
+        # for each token, replace '▁' with ' '
+        logprobs = {
+            token["token"].replace("▁", " "): token["logprob"] for token in logprobs
+        }
+        # change keys to list
+        # sort dictionary by values
+        sorted_logprobs = dict(
+            sorted(logprobs.items(), key=lambda x: x[1], reverse=True)
+        )
+        # TODO - automate this?
+        now = time.time()
+        min_time = 1
+        if now - start_time < min_time:
+            wait_time = min_time - (now - start_time)
+            wait_time = max(wait_time, 0)
+            time.sleep(wait_time)
+        return sorted_logprobs
+
+    def sample_several(self, prompt, temperature=0, n_tokens=10):
+        response = requests.post(
+            f"https://api.ai21.com/studio/v1/{self.engine}/complete",
+            headers={"Authorization": "Bearer " + str(self.API_KEY)},
+            json={
+                "prompt": prompt,
+                "numResults": 1,
+                "maxTokens": n_tokens,
+                "temperature": temperature,
+            },
+        )
+        response_dict = response.json()
+        text = response_dict["completions"][0]["data"]["text"]
+        return text
+
+
+if __name__ == "__main__":
+    lm = LM_JURASSIC("j1-jumbo")
+    probs = lm.send_prompt("What is the capital of France?\nThe capital of France is")
+    probs = lm.send_prompt(prompt)
+    print(probs)
